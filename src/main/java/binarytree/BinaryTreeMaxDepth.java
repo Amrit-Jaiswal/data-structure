1ba9ac58b8d800c2dcc4891c6d56fafedec44c42
Amrit <jaiswal.amrit@gmail.com>
2019-06-06 07:31:22 +0530

    Height of binary tree

---
+++ src/main/java/binarytree/BinaryTreeMaxDepth.java
@@ -0,0 +1,59 @@
+package binarytree;
+
+public class BinaryTreeMaxDepth {
+
+    public static void main(String[] args) {
+        BinaryTreeMaxDepth binaryTreeMaxDepth = new BinaryTreeMaxDepth();
+        Node<Integer> binaryTree = binaryTreeMaxDepth.createBinaryTree();
+        int maxDepth = binaryTreeMaxDepth.maxDepth(binaryTree);
+
+        System.out.println("Depth of Binary Tree is :" +maxDepth);
+
+    }
+
+    private int maxDepth(Node<Integer> root) {
+        if (root == null) {
+            return 0;
+        }
+        int lDepth = maxDepth(root.left);
+        int rDepth = maxDepth(root.right);
+
+        if (lDepth > rDepth) {
+            return lDepth + 1;
+        } else {
+            return rDepth + 1;
+        }
+    }
+
+    private Node<Integer> createBinaryTree(){
+        Node<Integer> n1 = new Node<>(10);
+        Node<Integer> n2 = new Node<>(20);
+        Node<Integer> n3 = new Node<>(30);
+        Node<Integer> n4 = new Node<>(5);
+        Node<Integer> n5 = new Node<>(15);
+        Node<Integer> n6 = new Node<>(8);
+        Node<Integer> n7 = new Node<>(9);
+        Node<Integer> n8 = new Node<>(22);
+        Node<Integer> n9 = new Node<>(58);
+        Node<Integer> n10 = new Node<>(50);
+        Node<Integer> n11 = new Node<>(40);
+
+        Node<Integer> root = n1;
+        n1.left = n2;
+        n1.right= n3;
+
+        n2.left = n4;
+        n2.right = n5;
+
+        n3.left = n6;
+        n3.right = n7;
+
+        n5.left = n8;
+        n8.right = n9;
+
+        n6.right = n10;
+        n10.right = n11;
+
+        return root;
+    }
+}
